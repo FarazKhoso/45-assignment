c40edf5086569d181e06a2f4ada6d17a51a97712
FarazKhoso <161611764+FarazKhoso@users.noreply.github.com>
2024-03-05 20:39:41 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-//*Question 1:* Install Node.js, TypeScript and VS Code on your computer/Laptop
+//Day 2
+//*Question 1:* Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+// "Albert Einstein once said, “A person who never made a mistake never tried anything new.”"
+var quote = "A person who never made a mistake never tried anything new";
+var author = "Albert Einstein";
+console.log("".concat(author, "once said, \"").concat(quote));
+// *Question 2:* Famous Quote 2: Repeat Exercise 1, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+var quote = "A person who never made a mistake never tried anything new.";
+var famousPerson = "Alber Enistin";
+var message = "".concat(famousPerson, "once said, ").concat(quote);
+console.log(message);
+// *Question 3:* Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Use "\t" (tab) and "\n" (new line) at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after stripping the white spaces.
+var personName = "\n\t Faraz\t\n";
+console.log(personName);
+var stripped = personName.trim();
+console.log(stripped);
+// day 2 is completed by Faraz Khoso
--- main.ts
+++ main.ts
@@ -1,24 +1,26 @@
-//*Question 1:* Install Node.js, TypeScript and VS Code on your computer/Laptop
+//Day 2
+ //*Question 1:* Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+// "Albert Einstein once said, “A person who never made a mistake never tried anything new.”"
+
+let quote: string= "A person who never made a mistake never tried anything new";
+let author : string= "Albert Einstein";
+console.log(`${author}once said, "${quote}`);
 
 
-//*Question 2:* Personal Message: Store a person’s name in a variable and print a message to them, like “Hello Asharib, would you like to learn some TypeScript today?”
 
-let personName : string = "ERIC";
-console.log(`hello ${personName}, would you like to learn some python today?`);
+// *Question 2:* Famous Quote 2: Repeat Exercise 1, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
 
-
-//*Question 3:* Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
-
-// lower case
-let personName:string = "Faraz"
-console.log("lowercase:", personName.toLowerCase());
-
-//UPPER CASE
-console.log("uppercase:", personName.toUpperCase());
+let quote: string= "A person who never made a mistake never tried anything new.";
+let famousPerson : string ="Alber Enistin";
+let message = `${famousPerson}once said, ${quote}`;
+console.log(message);
 
 
-// Title case
- console.log("titlecase:", personName.replace(/\bw/g,c => c.toUpperCase()));
- 
+// *Question 3:* Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Use "\t" (tab) and "\n" (new line) at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after stripping the white spaces.
 
-// day 1 is complete //
+let personName:string =`\n\t Faraz\t\n`;
+console.log(personName);
+let stripped : string = personName.trim();
+console.log(stripped);
+
+// day 2 is completed by Faraz Khoso
