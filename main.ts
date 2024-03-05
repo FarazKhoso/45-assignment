c657a702839ae1c9c4aa583c83d1d19ee2524010
FarazKhoso <161611764+FarazKhoso@users.noreply.github.com>
2024-03-05 19:48:08 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,24 @@
+//*Question 1:* Install Node.js, TypeScript and VS Code on your computer/Laptop
+
+
+//*Question 2:* Personal Message: Store a person’s name in a variable and print a message to them, like “Hello Asharib, would you like to learn some TypeScript today?”
+
+let personName : string = "ERIC";
+console.log(`hello ${personName}, would you like to learn some python today?`);
+
+
+//*Question 3:* Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+// lower case
+let personName:string = "Faraz"
+console.log("lowercase:", personName.toLowerCase());
+
+//UPPER CASE
+console.log("uppercase:", personName.toUpperCase());
+
+
+// Title case
+ console.log("titlecase:", personName.replace(/\bw/g,c => c.toUpperCase()));
+ 
+
+// day 1 is complete //
